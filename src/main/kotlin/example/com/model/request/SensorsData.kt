e564550c64f41427ac7a2535c0e0182017305a9c
Betucciny <betohr48@gmail.com>
2024-08-30 00:22:12 -0600

    Temporal

--- src/main/kotlin/example/com/model/request/SensorsData.kt
+++ src/main/kotlin/example/com/model/request/SensorsData.kt
@@ -1,5 +1,8 @@
 package example.com.model.request
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class SensorsData(
     val tank1: Int,
     val tank2: Int,
--- src/main/kotlin/example/com/plugins/Security.kt
+++ src/main/kotlin/example/com/plugins/Security.kt
@@ -4,7 +4,8 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 
 fun Application.configureSecurity() {
-    val token = environment.config.propertyOrNull("token")?.getString() ?: "1234"
+    val token = environment.config.propertyOrNull("storage.token")?.getString() ?: "1234"
+    println("Token: $token")
     install(Authentication){
         bearer("auth") {
             realm = "Ktor Server"
